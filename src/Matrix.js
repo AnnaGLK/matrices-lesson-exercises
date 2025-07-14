8c01e06eb875e0507fa87268caaefdcb8a976483
Anna Glikin <glikin.anna@gmail.com>
2025-07-15 02:01:52 +0300

    matrix start!

--- src/Matrix.js
+++ src/Matrix.js
@@ -1,6 +1,11 @@
 /* Write your code below */
+let m = new Matrix(3, 4)
+m.print()
 
 
+m.alter(0, 0, m.get(1, 1))
+m.printColumn(0) //prints 6, 5, 9 (separate lines)
+m.printRow(0) //prints 6, 2, 3, 4 (separate lines)
 
 
 //You can paste the code from the lesson below to test your solution
@@ -10,3 +15,4 @@
 
 /* Do not remove the exports below */
 module.exports = Matrix
+
